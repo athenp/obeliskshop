7d2e80b5fe37819e9a78acfa575a82bfaa2308cb
athenp <athenkai@hotmail.com>
2019-08-05 17:53:43 -0600

    splash

--- js/rotate.js
+++ js/rotate.js
@@ -1,8 +1,8 @@
 var scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xffffff );
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
+renderer.setClearColor( 0xffffff );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
