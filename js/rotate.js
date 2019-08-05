8bf9f9a2881c6b1b392f7931fada73c2ca5d2bcb
athenp <athenkai@hotmail.com>
2019-08-05 17:29:31 -0600

    splash

--- js/rotate.js
+++ js/rotate.js
@@ -5,8 +5,8 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var geometry = new THREE.BoxGeometry( 1, 3, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -14,8 +14,8 @@ camera.position.z = 5;
 
 function render() {
     requestAnimationFrame( render );
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+    cube.rotation.x += 0.0;
+    cube.rotation.y += 0.05;
     renderer.render( scene, camera );
 }
 render();
