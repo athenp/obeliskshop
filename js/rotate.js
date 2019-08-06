54f764d06cfa56f596420a636fa7e6a2571c8d8e
athenp <athenkai@hotmail.com>
2019-08-05 18:20:16 -0600

    splash

--- js/rotate.js
+++ js/rotate.js
@@ -5,22 +5,24 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var jsonLoader = new THREE.JSONLoader();
-jsonLoader.load("object.json", addModelToScene);
+var geometry = new THREE.BoxGeometry( 1, 3, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-function addModelToScene(geometry, materials) {
-    var material = new THREE.MeshFaceMaterial(materials);
-    var object = new THREE.Mesh(geometry, material);
-    object.scale.set(10, 10, 10);
-    scene.add(object);
-}
+var geometry = new THREE.CircleGeometry( 5, 32 );
+var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+var circle = new THREE.Mesh( geometry, material );
+scene.add( circle );
 
 camera.position.z = 5;
 
 function render() {
     requestAnimationFrame( render );
-    object.rotation.x += 0.0;
-    object.rotation.y += 0.05;
+    cube.rotation.x += 0.0;
+    cube.rotation.y += 0.05;
+    circle.rotation.x += 0.0;
+    circle.rotation.y += 0.05;
     renderer.render( scene, camera );
 }
 render();
