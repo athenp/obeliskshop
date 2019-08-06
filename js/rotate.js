c9ae7f1d4d00d70903a9a6798b6ae777e69b09f1
athenp <athenkai@hotmail.com>
2019-08-05 23:01:23 -0600

    splash

--- js/rotate.js
+++ js/rotate.js
@@ -10,11 +10,12 @@ var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-var geometry = new THREE.CylinderGeometry( .2, .2, 1.05, 32 );
+var geometry = new THREE.CylinderGeometry( .3, .3, 1.05, 32 );
 var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
 var cylinder = new THREE.Mesh( geometry, material );
 cylinder.rotateX( Math.PI / 2 );
 scene.add( cylinder );
+cylinder.position.set(0, 0, 1);
 
 camera.position.z = 5;
 
