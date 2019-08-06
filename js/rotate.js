66e1af2e061dc3b7df9c341fb0e55f73cc7756cd
athenp <athenkai@hotmail.com>
2019-08-06 13:59:00 -0600

    make it better

--- js/rotate.js
+++ js/rotate.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
-var container = document.getElementById('.canvas');
+var container = document.getElementByClassName('canvas');
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor((Math.random() * 0xffffff),1);
 container.appendChild(renderer.domElement);
